6b1c5049bc61afb33cec86417c9e71229dd4a95a
Pouria Damavandi <damavandipouria@gmail.com>
2024-10-27 19:55:12 +0330

    feat: define initial layout

--- src/components/layout/mainLayout/mainContainer/mainContainer.tsx
+++ src/components/layout/mainLayout/mainContainer/mainContainer.tsx
@@ -1,7 +1,7 @@
 import type { FC, ReactNode } from "react";
 
 const MainContainer: FC<{ children: ReactNode }> = ({ children }) => {
-  return <div className="my-12">{children}</div>;
+  return <div className="mx-4">{children}</div>;
 };
 
 export default MainContainer;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import MainContainer from "./components/layout/mainLayout/mainContainer/mainContainer";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import MainContainer from "./components/layout/mainLayout/mainContainer/mainContainer";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
